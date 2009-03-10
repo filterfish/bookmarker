5ea86f69f88e542f60c918c20526a78f4f3889af
Richard Heycock <rgh@roughage.com.au>
2009-03-11 08:44:03 +1100

    Remove tokyo cabinet, added xapian & added download code.
    Intial implementation with xapian. It also downloads the uri and adds
    the content to xapian; there is not cleaning of the html. Added to libs
    from topikality.

---
+++ lib/simplehttpclient.rb
@@ -0,0 +1,165 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+require 'zlib'
+require 'stringio'
+require 'net/http'
+require 'rubygems'
+require File.dirname(__FILE__) + '/uri_utilities'
+
+class SimpleHttpClient 
+
+  def initialize(user_agent='', timeout=nil)
+    @user_agent = user_agent
+    @timeout = timeout
+  end
+
+  def get(uri, headers={}, limit=5)
+    raise RuntimeError, 'HTTP redirect too deep' if limit == 0
+
+    u = URIUtilities.clean(uri)
+    return if u.nil?
+
+    req = (u.query.nil?) ? u.path : "#{u.path}?#{u.query}"
+
+    http = Net::HTTP.new(u.host, u.port)
+    request = Net::HTTP::Get.new(req)
+
+    # Set timeouts if needed.
+    if @timeout
+      http.open_timeout = @timeout
+      http.read_timeout = @timeout
+    end
+
+    request = set_headers(request, headers)
+    response = http.request(request)
+
+    case response
+    when Net::HTTPSuccess
+      response['location'] = u.to_s
+      uncompress_content(response)
+    when Net::HTTPNotModified
+      response['location'] = u.to_s
+      uncompress_content(response)
+    when Net::HTTPRedirection
+      get(URIUtilities.absolute(response['location'], u), {}, limit -1)
+    else
+      response
+    end
+  end
+
+  def head(uri, headers={})
+    u = URIUtilities.clean(uri)
+    return if u.nil?
+
+    req = (u.query.nil?) ? u.path : "#{u.path}?#{u.query}"
+
+    http = Net::HTTP.new(u.host, u.port)
+    request = Net::HTTP::Head.new(req)
+
+    # Set timeouts if needed.
+    if @timeout
+      http.open_timeout = @timeout
+      http.read_timeout = @timeout
+    end
+
+    request = set_headers(request, headers)
+    response = http.request(request)
+    response['location'] = u.to_s
+    response
+  end
+
+  private
+
+  # Check to make sure that any compression is dealt with.
+  def uncompress_content(response)
+
+    body = StringIO.new
+    body.write(response.body)
+    body.rewind
+
+    if response['content-encoding']
+      case response['content-encoding'].downcase
+      when 'gzip'
+        begin
+          new_body = Zlib::GzipReader.new(body).read
+          response.delete("content-encoding")
+        rescue Zlib::GzipFile::Error
+          # If there is an error assume the content-encoding is wrong and return as is.
+          body.rewind
+          new_body = body.read
+        end
+      when 'deflate'
+        begin
+          new_body = Zlib::Inflate.inflate(body.read)
+        rescue Zlib::DataError
+          # no luck with Zlib decompression. Let's try with
+          # raw deflate, like some broken browsers do.
+          # See http://www.ruby-forum.com/topic/136825
+          body.rewind
+          new_body = Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(body.read);
+        end
+        response.delete("content-encoding")
+      when "identity"
+        new_body = body.read
+      end
+    else
+      new_body = body.read
+    end
+    return response, new_body
+  end
+
+  # Sets up the various headers. By default it sets up compression,
+  # language, charset, user-agent and does the right things with
+  # Last-Modified and Etag. Any supplied headers will be applied as is
+  def set_headers(request,headers)
+
+    # Downcase all headers. This means that the user of this class can
+    # give headers in either case without it causing a problem.
+    headers = headers.inject({}) { |h, v| h[v.first.downcase] = v.last; h }
+
+    request.add_field('accept-encoding', 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3')
+    request.add_field('accept-language', 'en-us,en;q=0.5')
+    request.add_field('accept-charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
+
+    request.add_field('user-agent', @user_agent) if @user_agent
+
+    last_modified = headers.delete('last-modified')
+    etag = headers.delete('etag')
+
+    request.add_field('if-modified-since', last_modified) if last_modified
+    request.add_field('etag', etag) if etag
+
+    # Add any remaining headers
+    headers.each_pair { |k,v| request.add_field(k, v) }
+
+    return request
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+
+  uri = ARGV[0]
+  if uri.nil?
+    puts "usage: #{$0}: <uri>"
+    exit(1)
+  end
+
+  User_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
+
+  agent = SimpleHttpClient.new(User_agent, 20.0)
+
+  headers = agent.head(uri)
+  pp headers
+  pp headers['content-type']
+  pp headers.to_hash
+
+  headers = { "Last-Modified" => "Thu, 07 Feb 2008 03:29:21 GMT", "etag" => "d78447-31f40-14582640" }
+  response, response_body = agent.get(uri, headers)
+
+  pp response.to_hash
+  pp response.class
+  File.open("body", "w") do |f|
+    f.puts response_body
+  end
+end
---
+++ lib/uri_utilities.rb
@@ -0,0 +1,84 @@
+require 'rubygems'
+
+class URIUtilities
+
+  IMAGE_REGEX = /\bjpeg\b|\bjpg\b|\bgif\b|\bpng\b|\bapng\b|\bmng\b|\btiff\b|\bsvg\b/i
+  VIDEO_REGEX = /\bavi\b|\bmpg\b|\bmpeg\b|\bm1v\b|\bmp2\b|\bmp3\b|\bmpa\b|\bmpe\b|\bmpv2\b|\basf\b|\bwma\b|\bwmv\b|\bivf\b|\brm\b|\bra\b|\bram\b|\bmov\b|\bqt\b/
+
+  EXTENSION_REGEXS = [IMAGE_REGEX, VIDEO_REGEX]
+
+  # This is a very simple method that ensures that the supplied uri
+  # looks like a uri and converts it to an Addressable::URI.
+  def self.clean(uri)
+    # Make sure it's not already an Addressable::URI
+    return uri if uri.is_a?(Addressable::URI) || uri.nil?
+
+    (u = (/^http:\/\//.match(uri)) ? Addressable::URI.parse(uri) : Addressable::URI.heuristic_parse(uri)) rescue return nil
+    u.path = '/' if u.path.length == 0
+    return u
+  end
+
+  def self.absolute(uri, parent_uri)
+    uri = Addressable::URI.parse(uri) unless uri.is_a? Addressable::URI rescue return nil
+    parent_uri = Addressable::URI.parse(parent_uri) unless parent_uri.is_a? Addressable::URI
+
+    if uri.nil? || uri.scheme == "mailto"
+      return nil
+    elsif !uri.absolute?
+      if absolute_path?(uri.path)
+        path = uri.path
+      else
+        # This has been coded according to the spec in http://htmlhelp.com/faq/html/basics.html
+        parent_path = (/\/$/.match(parent_uri.path)) ? parent_uri.path : File.dirname(parent_uri.path)
+        parent_path = File.expand_path(parent_path)
+
+        path = File.expand_path(File.join(parent_path, uri.path))
+
+        # Make sure that a slash is appended unless there is a . in the basename of the uri
+        path = File.join(path, "/") unless File.basename(path).index(".")
+      end
+
+      cleaned_uri = parent_uri.dup
+      cleaned_uri.path = path
+      cleaned_uri.query = uri.query
+      cleaned_uri.fragment = uri.fragment
+
+      return cleaned_uri
+    else
+      return uri
+    end
+  end
+
+  # I wanted to write these dynamically but I don't how to dynamically create class methods
+  def self.is_an_image?(uri)
+    is_a?(IMAGE_REGEX, uri)
+  end
+
+  def self.is_a_video?(uri)
+    is_a?(VIDEO_REGEX, uri)
+  end
+
+  def self.absolute_path?(path)
+    !Regexp.new(/^\//).match(path).nil?
+  end
+
+  private
+  def self.is_a?(regex, uri)
+    uri = Addressable::URI.parse(uri) unless uri.is_a? Addressable::URI
+    regex.match(get_extension(uri.path)) != nil
+  end
+
+  def self.get_extension(path)
+    File.extname(path).sub(/^\./, '')
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  puts URIUtilities.clean("smh.com.au")
+  puts URIUtilities.clean("www.smh.com.au")
+  puts URIUtilities.clean("http://smh.com.au")
+  puts URIUtilities.clean("smh.com.au/?poo")
+
+  puts URIUtilities.absolute("bob", "http://smh.com.au")
+  puts URIUtilities.absolute("one/two/", "http://www.foxsports.com.au/rss")
+end
